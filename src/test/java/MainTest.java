4111d01683c998e76fcb41cf27783a3a6e8b933c
aholban <andreeaholban98@gmail.com>
2021-10-18 11:17:50 +0100

    Created a test for Main Class

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,25 @@
+
+import com.softwareinstitute.andreeaholban.Member;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.softwareinstitute.andreeaholban.Main.findMember;
+import static org.junit.Assert.assertEquals;
+
+
+public class MainTest {
+    @Test
+    public void testFindMember(){
+        Member person = new Member("Andreea Holban", "12345");
+        Member person2 = new Member("Henry Barrington", "54321");
+        List<Member> members = new ArrayList<>();
+        members.add(person);
+        members.add(person2);
+        Member visitor = findMember(members, person.getName(), person.getCardID());
+        assertEquals(person.getName(), visitor.getName());
+        assertEquals(person.getCardID(), person.getCardID());
+    }
+}
